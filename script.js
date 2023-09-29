624e1185f40bd04d49c7760321af6d202eeb65c2
Jiří Gorčík <jirigorcik@seznam.cz>
2023-09-29 21:50:17 +0200

    Adding button which can resize grid

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const mainBox = document.querySelector(".main-box")
+const griSize = document.querySelector("#grid-size")
+const gridSizeLabel = document.querySelector(".grid-size-label")
+const newGrid = document.querySelector(".new-grid")
 
+
+griSize.addEventListener("input", () => {
+    let size = griSize.value
+    gridSizeLabel.textContent = `${size}x${size}`
+})
 
 
 const makeGrid = (rows, cols) => {
@@ -15,4 +23,18 @@ const makeGrid = (rows, cols) => {
     }
 }
 
+
+newGrid.addEventListener("click", () => {
+    let size = griSize.value
+    
+    while (mainBox.firstChild) {
+    mainBox.removeChild(mainBox.firstChild)
+    }
+    
+    makeGrid(size,size)
+})
+
+
 makeGrid(16,16)
+
+
