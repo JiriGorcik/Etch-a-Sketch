6402645afe98c4f9964849b1c552a2c537b1209b
Jiří Gorčík <jirigorcik@seznam.cz>
2023-09-29 18:52:42 +0200

    Adding funcion to set how big should be the grid and change color on black on hover

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const mainBox = document.querySelector(".main-box")
 
 
 
-for (let i = 0; i < 50; i++) {
-    let box = document.createElement("div")
-    box.classList.add("box")
-    mainBox.appendChild(box)
+const makeGrid = (rows, cols) => {
+    mainBox.style.setProperty("--grid-rows", rows)
+    mainBox.style.setProperty("--grid-cols", cols)
+    for (let i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement("div")
+        cell.addEventListener("mouseenter", () => {
+            cell.style.backgroundColor = "black"
+        })
+        mainBox.appendChild(cell).className = "grid-item"
+        
+    }
 }
+
+makeGrid(16,16)
